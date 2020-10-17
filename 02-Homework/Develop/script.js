d9866c8ca0eca5315c6688ca79db8873331d0d76
Alheli Miranda <alhealhe72@hotmail.com>
2020-10-16 21:51:01 -0400

    password2

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,76 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  console.log(password)
+
+}
+function generatePassword() {
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var number = "0123456789"
+  var special = "@!$%&/()=?,'*^-_*+·#<>{}[]|;:`"
+  var password =""
+
+  myLength = 0
+  for(;myLength < 8 || myLength > 128;) {
+   myLength = prompt("Ingresa la longitud de tu password, debe ser entre 8 y 128 caracteres? ")
+  console.log(myLength)
+}
+
+  var myLower = false;
+  var myUpper= false;
+  var mySpecial= false;
+  var myNumber= false;
+  for(;(myLower||myUpper||mySpecial)=== false;){
+    alert("Al menos debes elegir una de las siguiente opciones:")
+    myLower = confirm("quieres que incluya minusculas? ")
+    console.log(myLower)
+    myUpper = confirm("quieres que incluya mayusculas? ")
+    console.log(myUpper)
+    mySpecial = confirm("quieres que incluya carácteres especiales? ")
+    console.log(mySpecial)
+    myNumber = confirm("quieres que incluya números? ")
+    console.log(myNumber)
+  }
+  
+  myOptions = []
+  if (myLower===true){
+    myOptions.push(lower)
+    console.log(myOptions)
+  }
+  if (myUpper===true){
+    myOptions.push(upper)
+    console.log(myOptions)
+  }
+  if (mySpecial===true){
+    myOptions.push(special)
+    console.log(myOptions)
+  }
+  if (myNumber===true){
+    myOptions.push(number)
+    console.log(myOptions)
+  }
+  
+  
+  charType= Math.floor(Math.random() * myOptions.length );
+  for(i=0 ; i <= myLength ; i++)
+  {
+    charType= Math.floor(Math.random() * myOptions.length )
+    myOptions[charType]
+    charSelected  = Math.floor(Math.random() *myOptions[charType].length)
+    password = password + myOptions[charType][charSelected]
+    console.log(password)
+    
+  }
+  return password
+}
+
+writePassword() 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # secure_password
 make a random secure password
+
+# UNIT 03 JavaScript: Password Generator
+
+##  Task
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+
+## Process
+
+```
+FOR generate a secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I should answer each prompt, must accept at least one character type
+THEN my input should be validated
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is written to the page
+```
+
+## The application
+
+The following images shows the web application's series of prompts, appearance and functionality in order to obtain the password:
+1.
+![password generator demo](02-Homework/Assets/length.png);
+
+2.
+![password generator demo1](02-Homework/Assets/at_least.png);
+
+3.
+![password generator demo2](02-Homework/Assets/upper.png);
+
+4.
+![password generator demo3](02-Homework/Assets/lower.png);
+
+5.
+![password generator demo4](02-Homework/Assets/special.png);
+
+6.
+![password generator demo5](02-Homework/Assets/numbers.png);
+
+7.
+![password generator demo6](02-Homework/Assets/password.png);
+
+
+## Credits 
+
+To solve the needs to this subject, I use the following pages 
+- https://www.w3schools.com/
+- https://www.dyn-web.com/
+- https://developer.mozilla.org
+
+## Challenge
+
+It was very difficult for me, because I am beginning to understand how to code, and javascript is certainly a challenge
+
+## Contributing
+- https://github.com/AlheliMi/secure_password.git
+-  https://alhelimi.github.io/secure_password/.
+
+
+
+
+
+
